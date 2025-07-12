3dbce757eb7748f154d7b64c58e6e9a2e22d601d
akezhev <akezh@mail.ru>
2025-07-12 20:42:22 +0300

    changes some file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const tasksList = document.querySelector('#tasksList');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+
+
+});
+
