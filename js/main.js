3c5e0b3e8aea330b1fba839c05ce639cabc449dd
akezhev <akezh@mail.ru>
2025-07-15 21:29:25 +0300

    nothing else matters

--- js/main.js
+++ js/main.js
@@ -6,8 +6,17 @@ const tasksList = document.querySelector('#tasksList');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
-
-
+    const task = input.value;
+    if (task) {
+        addTask(task);
+        input.value = '';
+    }
 });
+
+function addTask(task) {
+    const taskItem = document.createElement('li');
+    taskItem.classList.add('task-item');
+    taskItem.innerHTML = task;
+    tasksList.appendChild(taskItem);
+}
 
