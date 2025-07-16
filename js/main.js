701a52ed9317867938a9d12a5e17da137bf1124b
akezhev <akezh@mail.ru>
2025-07-16 13:41:30 +0300

    added several properties to the main.js file

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
-
 const form = document.querySelector('#form');
-const input = document.querySelector('#input');
-const tasksList = document.querySelector('#tasksList');
+const taskInput = document.querySelector('#taskInput');
+const taskList = document.querySelector('#taskList');
 
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function(event) {
+    // Отменяем стандартное поведение формы
     event.preventDefault();
-    const task = input.value;
-    if (task) {
-        addTask(task);
-        input.value = '';
+
+    // Проверяем, есть ли текст в input
+    if (!taskInput.value) {
+        return;
     }
+
+
+    // Получаем текст задачи из input
+    const taskText = taskInput.value;
+
+    console.log(taskText);
+
+    // Создаем HTML для новой задачи
+    const taskHTML = `
+        <li class="list-group-item d-flex justify-content-between task-item">
+            <span class="task-title">${taskText}</span>
+            <div class="task-item__buttons">
+                <button type="button" data-action="done" class="btn-action">
+                    <img src="./img/done.svg" alt="Done" width="28" height="28">
+                </button>
+                <button type="button" data-action="delete" class="btn-action">
+                    <img src="./img/delete.svg" alt="Delete" width="28" height="28">
+                </button>
+            </div>
+        </li>
+    `;
+
+    // Добавляем задачу в список    
+    taskList.insertAdjacentHTML('beforeend', taskHTML);
+
+    // Очищаем input
+    taskInput.value = '';
+    
+    
 });
 
-function addTask(task) {
-    const taskItem = document.createElement('li');
-    taskItem.classList.add('task-item');
-    taskItem.innerHTML = task;
-    tasksList.appendChild(taskItem);
-}
 
